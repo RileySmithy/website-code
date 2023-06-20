216443b70d4f1588882dc0a5f05e4c180e60bc06
RileySmithy <134114772+RileySmithy@users.noreply.github.com>
2023-06-20 20:18:52 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function redirect() {
+    window.location.assign('aboutUs.html');
+}
+function redirect2() {
+    window.location.assign('products.html');
+}
+function redirect3() {
+    window.location.assign('contact.html');
+}
+function redirect4() {
+    window.location.assign('cart.html');
+}
